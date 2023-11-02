b9ac6338e0e62c17b63c0647138800862b1e5af6
akankshawagh15 <68599265+akankshawagh15@users.noreply.github.com>
2023-11-03 01:49:45 +0530

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,8 @@
+class Message{  
+    public static void main(String[] args){  
+    System.out.println("Jenkins is a powerful application that allows continuous integration and continuous delivery of projects, regardless of the platform you are working on. "); 
+    System.out.println("Jenkins is a software that allows continuous integration.");
+    System.out.println("Jenkins assignment.");
+    System.out.println("SDM tutorial");
+    }  
+    }  
